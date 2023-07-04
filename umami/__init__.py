085a430d1644ba12896b85145a45a1f6b151251f
Davide Marchegiani <davide.marchegiani@gmail.com>
2023-07-04 11:30:13 +1000

    Fix package build

--- umami/__init__.py
+++ umami/__init__.py
@@ -1,5 +1,5 @@
 # Copyright 2022 ACCESS-NRI and contributors. See the top-level COPYRIGHT file for details.
 # SPDX-License-Identifier: Apache-2.0
 
-__version__ = "v0.0.2"
+__version__ = "2023.02.8"
 
