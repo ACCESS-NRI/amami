bd0eef42b5f4a985cfaf7cc599bfe6ca1e177863
Ben Davies <ben.davies@anu.edu.au>
2024-04-22 15:53:54 +1000

    Reformat amami.py base module.

--- amami/amami.py
+++ amami/amami.py
@@ -10,26 +10,25 @@ Module to define main class and entry point for CLI usage of `amami`.
 import sys
 from amami.parsers.main_parser import MainParser
 
+
 class Amami:
     """A class that represents the `amami` application."""
-    def __init__(
-        self,
-        argv: list[str],
-    ) -> None:
-        self.args = MainParser().parse_and_process(
-            argv[1:] if argv[1:] else ["-h"]
-        )
+
+    def __init__(self, argv: list[str]):
+        self.args = MainParser().parse_and_process(argv[1:] if argv[1:] else ["-h"])
 
     def run_command(self):
         """Main function for `amami`."""
-        command = getattr(self.args, 'subcommand')
+        command = getattr(self.args, "subcommand")
         from importlib import import_module
+
         mainfun = getattr(
-            import_module(f'amami.core.{command}'),
-            'main',
+            import_module(f"amami.core.{command}"),
+            "main",
         )
         # Call 'main' function of chosen command
         mainfun(self.args)
+
 
 def main() -> None:
     """Entry point for CLI usage of `amami`."""
